b2ff93b07bf74b6708d610fc6d8273986b790478
LadyMalande <42152093+LadyMalande@users.noreply.github.com>
2019-03-26 16:38:03 +0100

    Update HelloWorld.java
    Opraven pozdrav

--- src/cz/jankuroman/java/mff/git/HelloWorld.java
+++ src/cz/jankuroman/java/mff/git/HelloWorld.java
@@ -2,6 +2,7 @@ package cz.jankuroman.java.mff.git;
 
 public class HelloWorld {
     public static void main (String[] args){
-        System.out.println("Chybička");
+        System.out.println("Chybička"); // Jejda
+        System.out.println("Nazdar sveteee!!!");
     }
 }
